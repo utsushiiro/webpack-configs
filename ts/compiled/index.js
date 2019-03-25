bb83e365623c90c3ff312be5a09f609684222565
utsushiiro <ozeki.haru@gmail.com>
2019-03-25 20:44:42 +0900

    webpack + tsのconfigを作成

---
+++ ts/compiled/index.js
@@ -0,0 +1,4 @@
+var message = '';
+message = "TypeScript World";
+console.log("Hello!" + message);
+//# sourceMappingURL=index.js.map
---
+++ ts/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // ソースマップ有効(productionで無効)
+    mode: "development",
+  
+    entry: "./src/index.ts",
+  
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader"
+        }
+      ]
+    },
+    // importの際に.tsを見るように
+    resolve: {
+      extensions: [".ts"]
+    }
+  };
